bb5ee9d985b626bf19baaf0abbb48af6742c68a9
kgilbert <kerrigan.gilbert@gmail.com>
2016-06-23 15:20:48 -0500

    Completed script

--- mappingStats_summary.Rformat.pl
+++ mappingStats_summary.Rformat.pl
@@ -20,13 +20,50 @@ my (%opt, $directory, $filename, $outfile);
 getopts('d:f:o:', \%opt);
 &var_check();
 
+opendir (DIR, $directory) or die "Could not open Dir $directory: $!\n";
+my @dir_files = readdir DIR;
+closedir DIR;
 
+open (OUT, ">$outfile") || die "Cannot open $outfile: $!\n\n";
 
+my (%hash);
 
+foreach my $in_file (@dir_files) {
+     chomp $in_file;
+     next if ($in_file !~ /$filename/);
+     
+     open (IN, "$directory/$in_file") || die "Cannot open $in_file: $!\n\n";
+     while (my $line = <IN>) {
+          chomp $line;
+          next if ($line =~ /^File/);
+          
+          my @split = split(/\t/, $line);
+          my $name = $split[0];
+          $name =~ s/\.sam//;
+          
+          my $parsed = $split[1];
+          my $mapped = $split[3];
+          
+          $hash{$name}{'parsed'} = $parsed;
+          $hash{$name}{'mapped'} = $mapped;
+     }
+     close IN;
+}
 
+my @types = qw (parsed mapped);
 
+print OUT "File\tData\tType\n";
+foreach my $file (sort {$a cmp $b} keys %hash) {
+     foreach my $type (@types) {
+          print OUT "$file\t$hash{$file}{$type}\t$type\n";
+     }
+}
+close OUT;
+exit;
 
-
+########################################################
+##                var_check Subroutine               ##
+########################################################
 sub var_check {
 	if ($opt{'d'}) {
 		$directory = $opt{'d'};
@@ -45,6 +82,9 @@ sub var_check {
 	}
 }
 
+########################################################
+##                var_error Subroutine               ##
+########################################################
 sub var_error {
 	print "@script_info";
 	exit 0;
